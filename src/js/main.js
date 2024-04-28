90ea5c6815448e0d8f2b673f66d7038b481fbb84
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2024-04-28 20:34:06 +0300

    s

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+// import posts from "../posts.json";
+// import postTemplate from '../templates/post-template.hbs';
+// const wrap = document.querySelector(".wrap")
+// function markup(books) {
+//     wrap.innerHTML = templateFunction({books});   
+// }
+// markup(books)
+// const search = document.querySelector(".search") 
+// function onSearch (event){
+//     const order = event.currentTarget.value
+//     console.log(order)
+//     if (order !== "") {
+//         const filteredBooks = books.filter(book => book.name.toLowerCase().includes(order.toLowerCase()))
+//         markup(filteredBooks)
+//     } else {
+//         markup(books)
+//     }
+// }
+// search.addEventListener("change", onSearch)
